0d8b022b5f8a6f6e5e25fb706b1bac982b8e4478
Josep Conejero <bermeu@gmail.com>
2022-02-27 12:36:08 +0100

    Added index.js to database folder

---
+++ src/database/index.js
@@ -0,0 +1,16 @@
+const debug = require("debug")("resdocial:database");
+const mongoose = require("mongoose");
+
+const connectToDataBase = (connectionString) =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(connectionString, (error) => {
+      if (error) {
+        reject(new Error(`You can't connect a database: ${error.message}`));
+        return;
+      }
+      debug("You're connected a database");
+      resolve();
+    });
+  });
+
+module.exports = connectToDataBase;
